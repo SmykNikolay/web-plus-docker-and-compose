8d5fb0dcc120b6b3277ab55f053e6f4eea35ca46
SmykNikolay <157750278+SmykNikolay@users.noreply.github.com>
2024-06-07 23:13:19 +0300

    init

--- backend/src/configuration.ts
+++ backend/src/configuration.ts
@@ -1,12 +1,12 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
-    type: process.env.DATABASE_TYPE,
-    host: process.env.DATABASE_HOST,
+    type: process.env.DATABASE_TYPE || 'postgres',
+    host: process.env.DATABASE_HOST || 'database',
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    name: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME || 'student',
+    password: process.env.DATABASE_PASSWORD || 'student',
+    name: process.env.DATABASE_NAME || 'kupipodariday',
   },
   SECRET_KEY: process.env.SECRET_KEY || 'SECRET_KEY',
 });
